311c6be4dfd687960d59d951fc6e2dad0344f761
unknown <ajibadeifedolapo17@gmail.com>
2024-07-08 16:51:17 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-blue-700 font-barlow">Let's get this shii started</h1>
-      <h1 className="text-red-700 font-bellefair">Let's get this shii started</h1>
-      <h1 className="text-green-700 font-barlow_bondensed">Let's get this shii started</h1>
+      <h1 className="text-blue-700 font-barlow">Lets get this shii started</h1>
+      <h1 className="text-red-700 font-bellefair">Lets get this shii started</h1>
+      <h1 className="text-green-700 font-barlow_bondensed">Lets get this shii started</h1>
     </main>
   );
 }
